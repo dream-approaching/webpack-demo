bd41c03242a98883d59125ca647eb01e84415786
郑金寿 <891661163@qq.com>
2018-08-13 14:21:54 +0800

    执行npx webpack --config webpack.config.js生成bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require("path");
+const path = require('path');
 
-module.export = {
-  entry: "./src/index.js",
+module.exports = {
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
 };
